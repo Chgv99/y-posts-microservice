3f9b394e0c1541dc0ba43b00549f62d48149b2ea
Christian G <christiangv99@gmail.com>
2026-01-07 03:52:20 +0000

    Update import

--- src/main/java/com/chgvcode/y/posts/dto/GetPostResponse.java
+++ src/main/java/com/chgvcode/y/posts/dto/GetPostResponse.java
@@ -2,8 +2,6 @@ package com.chgvcode.y.posts.dto;
 
 import java.time.Instant;
 
-import com.chgvcode.y.posts.client.dto.UserResponse;
-
 public record GetPostResponse(
     String message,
     UserResponse author,
