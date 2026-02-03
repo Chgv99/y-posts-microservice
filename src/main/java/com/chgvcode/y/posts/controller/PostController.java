af6c3d7fddc7b2f14be4f9f50177472972fcac9c
Christian G <christiangv99@gmail.com>
2026-02-03 22:54:54 +0000

    Map response in controller

--- src/main/java/com/chgvcode/y/posts/controller/PostController.java
+++ src/main/java/com/chgvcode/y/posts/controller/PostController.java
@@ -1,8 +1,10 @@
 package com.chgvcode.y.posts.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -19,10 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.chgvcode.y.posts.dto.CreatePostResponse;
 import com.chgvcode.y.posts.dto.GetPostResponse;
 import com.chgvcode.y.posts.dto.PostRequest;
+import com.chgvcode.y.posts.entity.Post;
+import com.chgvcode.y.posts.entity.User;
+import com.chgvcode.y.posts.mapper.PostMapper;
 import com.chgvcode.y.posts.service.PostService;
+import com.chgvcode.y.posts.service.UserService;
 
 import lombok.RequiredArgsConstructor;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -31,31 +36,43 @@ public class PostController {
 
     private final PostService postService;
 
+    private final UserService userService;
+
+    private final PostMapper postMapper;
+
     @GetMapping
     public Page<GetPostResponse> getPosts(
             @RequestParam(required = false) String authorUsername,
             @RequestParam(required = false) UUID authorUuid,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "createdAt,desc") String sort
-    ) {
+            @RequestParam(defaultValue = "createdAt,desc") String sort) {
         String[] sortParams = sort.split(",");
         Pageable pageable = PageRequest.of(
                 page,
                 size,
-                Sort.by(Sort.Direction.fromString(sortParams[1]), sortParams[0])
-        );
+                Sort.by(Sort.Direction.fromString(sortParams[1]), sortParams[0]));
+        Page<Post> postPage;
         if (authorUsername != null && !authorUsername.isEmpty()) {
-            return postService.getPostsByUsername(authorUsername, pageable);
+            postPage = postService.getPostsByUsername(authorUsername, pageable);
         } else if (authorUuid != null) {
-            return postService.getPostsByUuid(authorUuid, pageable);
+            postPage = postService.getPostsByUuid(authorUuid, pageable);
+        } else {
+            postPage = postService.getPosts(pageable);
         }
-        return postService.getPosts(pageable);
+        
+        List<GetPostResponse> getPostResponses = postPage.getContent().stream().map(post -> {
+            User user = userService.getUserByUuid(post.authorUuid());
+            return postMapper.toGetPostResponse(post, user);
+        }).toList();
+
+        return new PageImpl<>(getPostResponses, pageable, postPage.getTotalElements());
     }
 
     @PostMapping
-    public ResponseEntity<CreatePostResponse> createPost(@RequestHeader("X-User-Uuid") UUID userUuid, @RequestBody PostRequest request) {
+    public ResponseEntity<CreatePostResponse> createPost(@RequestHeader("X-User-Uuid") UUID userUuid,
+            @RequestBody PostRequest request) {
         CreatePostResponse postResponse = postService.createPost(request.message(), userUuid);
         return new ResponseEntity<>(postResponse, HttpStatus.CREATED);
-    }  
+    }
 }
